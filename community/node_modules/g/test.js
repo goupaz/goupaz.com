12004376f49638a4ef644bf0bd042c96bb3df43f
Sako <sako@opengov.com>
2020-09-24 18:31:54 -0700

    add

--- community/node_modules/g/test.js
+++
@@ -1,7 +0,0 @@
-
-
-require("./index.js")("util");
-
-console.log(inspect({"Woot!":"Test complete."}));
-
-
--- community/node_modules/node-sass-chokidar/test/fixtures/extras/my_custom_functions_string_conversion.js
+++
@@ -1,8 +0,0 @@
-var sass = require("node-sass");
-
-module.exports = {
-    "foo($a)": function (str) {
-        str = str.getValue().replace(/['"]/g, "");
-        return new sass.types.String('"' + str + str + '"');
-    },
-};
