bcbc8ba93148738abe1044c39a0470d36cc91292
Arlington1985 <arlington1985@gmail.com>
2021-01-03 00:27:01 +0100

    format js code

--- community/src/assets/persons.js
+++ community/src/assets/persons.js
@@ -1,67 +1,68 @@
-import { v4 as uuidv4 } from "uuid"
+import {
+    v4 as uuidv4
+} from "uuid"
 
 export const data = {
-  people: [
-{
-      id: uuidv4(),
-      name: "Sako M",
-      img: "https://avatars3.githubusercontent.com/u/43357548?v=4",
-      links: {
-        website: "https://goupaz.com",
-        linkedin: "https://www.linkedin.com/in/sakom/",
-        github: "https://github.com/sakomws",
-      },
-      jobTitle: "DevOps",
-      location: {
-        city: "San Francisco",
-        state: "California",
-        country: "USA",
-      },
-    },
- {
-      id: uuidv4(),
-      name: "Nurlana Mollayeva",
-      img: "https://avatars2.githubusercontent.com/u/74115466?v=4",
-      links: {
-        website: "https://goupaz.com",
-        linkedin: "https://www.linkedin.com/in/nurlana1/",
-        github: "https://github.com/Nurlana01",
-      },
-      jobTitle: "Community Manager",
-      location: {
-        city: "Baku",
-        country: "Azerbaijan",
-      },
-    },
- {
-      id: uuidv4(),
-      name: "Emin Ghuliev",
-      img: "https://i.imgur.com/ijV4k9L.jpg",
-      links: {
-        website: "https://goupaz.com",
-        linkedin: "https://www.linkedin.com/in/emin-ghuliev-461a22129/",
-        github: "https://github.com/eminghuliev",
-      },
-      jobTitle: "Community Leader",
-      location: {
-        city: "Baku",
-        country: "Azerbaijan",
-      },
-    },
-{
-      id: uuidv4(),
-      name: "Rasim Abdullayev",
-      img: "hhttps://avatars2.githubusercontent.com/u/56521004?v=4",
-      links: {
-        website: "https://goupaz.com",
-        linkedin: "https://www.linkedin.com/in/abdullayevrs/",
-        github: "https://github.com/rasimab",
-      },
-      jobTitle: "Community Leader",
-      location: {
-        city: "Baku",
-        country: "Azerbaijan",
-      },
-    },
-  ],
+    people: [{
+            id: uuidv4(),
+            name: "Sako M",
+            img: "https://avatars3.githubusercontent.com/u/43357548?v=4",
+            links: {
+                website: "https://goupaz.com",
+                linkedin: "https://www.linkedin.com/in/sakom/",
+                github: "https://github.com/sakomws",
+            },
+            jobTitle: "DevOps",
+            location: {
+                city: "San Francisco",
+                state: "California",
+                country: "USA",
+            },
+        },
+        {
+            id: uuidv4(),
+            name: "Nurlana Mollayeva",
+            img: "https://avatars2.githubusercontent.com/u/74115466?v=4",
+            links: {
+                website: "https://goupaz.com",
+                linkedin: "https://www.linkedin.com/in/nurlana1/",
+                github: "https://github.com/Nurlana01",
+            },
+            jobTitle: "Community Manager",
+            location: {
+                city: "Baku",
+                country: "Azerbaijan",
+            },
+        },
+        {
+            id: uuidv4(),
+            name: "Emin Ghuliev",
+            img: "https://i.imgur.com/ijV4k9L.jpg",
+            links: {
+                website: "https://goupaz.com",
+                linkedin: "https://www.linkedin.com/in/emin-ghuliev-461a22129/",
+                github: "https://github.com/eminghuliev",
+            },
+            jobTitle: "Community Leader",
+            location: {
+                city: "Baku",
+                country: "Azerbaijan",
+            },
+        },
+        {
+            id: uuidv4(),
+            name: "Rasim Abdullayev",
+            img: "hhttps://avatars2.githubusercontent.com/u/56521004?v=4",
+            links: {
+                website: "https://goupaz.com",
+                linkedin: "https://www.linkedin.com/in/abdullayevrs/",
+                github: "https://github.com/rasimab",
+            },
+            jobTitle: "Community Leader",
+            location: {
+                city: "Baku",
+                country: "Azerbaijan",
+            },
+        },
+    ],
 }
